969e1fe3a0d2c4767bc4dd58ea576e7e69acd57e
Balkrishna Toraskar <118865365+BalTor02@users.noreply.github.com>
2026-09-12 23:14:26 +0530

    Add files via upload

---
+++ Core/AchievementDefinition.cs
@@ -0,0 +1,16 @@
+﻿namespace UbiSlot.Core;
+
+public class AchievementDefinition
+{
+    public uint AchievementId { get; init; }
+
+    public string Name { get; init; } = string.Empty;
+
+    public string Description { get; init; } = string.Empty;
+
+    public string? IconPath { get; init; }
+
+    public bool IsUnlocked { get; set; }
+
+    public DateTime? UnlockTimeUtc { get; set; }
+}
---
+++ Core/SpoolRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace UbiSlot.Core;
+
+public class SpoolRecord
+{
+    public uint AchievementId { get; init; }
+
+    public long UnlockTimestamp { get; init; }
+
+    public byte[] RawBytes { get; init; } = [];
+
+    public DateTime UnlockTimeUtc =>
+        DateTimeOffset
+            .FromUnixTimeSeconds(UnlockTimestamp)
+            .UtcDateTime;
+}
